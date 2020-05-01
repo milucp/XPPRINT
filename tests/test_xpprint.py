e510d560e8f466ae5ab602bbf41b9d9a6c85f357
milucp <43660457+milucp@users.noreply.github.com>
2020-05-01 16:40:42 +0900

    スケルトンを追加

---
+++ tests/test_xpprint.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `xpprint` package."""
+
+
+import unittest
+
+from xpprint import xpprint
+
+
+class TestXpprint(unittest.TestCase):
+    """Tests for `xpprint` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
---
+++ xpprint/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for XPPRINT."""
+
+__author__ = """milucp"""
+__email__ = 'None'
+__version__ = '0.1.0'
